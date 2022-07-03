8e6e2a1ef84fdc21c08e858aafb67e232b6d210f
dgrammatiko <d.grammatiko@gmail.com>
2022-07-04 01:12:11 +0200

    cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,16 @@
 const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy');
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addNunjucksFilter('activeStation',
-      function(array, station){
-        const x = array.filter(x => x.code === station);
-        if (x[0]) {
-          return x[0];
-        }
-       return {}
-      }
-    );
+  eleventyConfig.addNunjucksFilter('activeStation', function(array, station) {
+    const x = array.filter(x => x.code === station);
+    if (x.length) {
+      return x[0];
+    }
+    return {}
+  });
 
-    eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: 'serverless',
     functionsDir: './netlify/functions/',
-    copy: [
-      { from: '_includes', to: '_includes' },
-      { from: '_data', to: '_data' }
-    ],
   });
 };
